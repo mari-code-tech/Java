cd5a5d2514bb128b25a712145b3cfc888a31a85d
PC <rete.anamaria@gmail.com>
2019-09-11 20:52:34 +0300

    com

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Wilkommen!");
+
+        System.out.println("Das ist nur ein Beispiel. Das ist meine erte Versuchung um Java zu lernen.");
+        int Inteligencelevel = 156436890;
+
+    }
+}
