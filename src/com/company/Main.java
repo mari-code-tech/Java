b2769aab366b3d52ab91412dad23764784e6c209
PC <rete.anamaria@gmail.com>
2019-09-17 22:00:24 +0300

    homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,18 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Wilkommen!");
+        System.out.println("Hallo!");
 
-        System.out.println("Das ist nur ein Beispiel. Das ist meine erte Versuchung um Java zu lernen.");
-        int Inteligencelevel = 156436890;
+        System.out.println("Ana-Maria");
+        System.out.println(2 + 6);
+        System.out.println(7/3f);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8f);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+
+
 
     }
 }
